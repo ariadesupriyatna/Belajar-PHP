2812df66c36852263c13a661c3ad8559397981c7
code27 <88775244+ariadesupriyatna@users.noreply.github.com>
2023-02-22 08:14:55 +0700

    Add files via upload

---
+++ 4Multi-Line-Comments.php
@@ -0,0 +1,10 @@
+<?php   
+   echo "<p>sedang apa kamu!</p>";
+    /*
+    ini banyak komen
+    biasanya ya gini buat nerangin panjang lebar
+    hehe
+    */
+    echo "<p>saya sedang belajar pemograman PHP!</p>";
+    echo "<p>ini adalah program pertama saya!</p>";
+?>
