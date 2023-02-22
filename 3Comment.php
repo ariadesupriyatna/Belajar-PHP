e9267ae2ba7c51dee8ca7329392d49fe23d15b6c
code27 <88775244+ariadesupriyatna@users.noreply.github.com>
2023-02-22 08:14:35 +0700

    Add files via upload

---
+++ 3Comment.php
@@ -0,0 +1,6 @@
+<?php   
+    echo "<p>sedang apa kamu!</p>";
+    // satu komentar begini doang ya singkat
+    echo "<p>saya sedang belajar pemograman PHP!</p>";
+    echo "<p>ini adalah program pertama saya!</p>";
+?>
